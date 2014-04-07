b4e77cf0849187f34d8f0212d809b79dfa110a77
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-06 20:54:22 -0400

    Send mail via script.
    The script now sends an email with the received data.

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -4,7 +4,16 @@
 		
 		$email = json_decode($json, true);
 		
-		print_r($email);
+		$headers = 'From: '.$email['from']."\r\n".
+		    'Reply-To: '.$email['from']."\r\n".
+		    'X-Mailer: PHP/'.phpversion();
+
+		if (mail($email['to'], $email['subject'], $email['message'], $headers)) {
+			echo "Your message has successfully been sent.";
+		}
+		else {
+			echo "Your message was not sent, please try again later.";
+		}
 	}
 	else {
 		echo "No data.";
